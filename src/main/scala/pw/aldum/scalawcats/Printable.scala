c1880edf8ecbd881acdedca4d4e9aab367445177
aldum <aldum@artixlinux.org>
2023-01-17 09:37:13 +0100

    1.3.2

--- src/main/scala/pw/aldum/scalawcats/Printable.scala
+++ src/main/scala/pw/aldum/scalawcats/Printable.scala
@@ -3,3 +3,10 @@ package scalawcats
 
 trait Printable[A]:
   def format[A](a: A): String = a.toString
+
+object PrintableInstances:
+  given Printable[String] with
+    def format(s: String): String = s
+
+  given Printable[Int] with
+    def format(i: Int): String = i.toString
