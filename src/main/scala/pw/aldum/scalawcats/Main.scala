9917ff4e8791e0315e31251490790a44552d0b46
aldum <aldum@artixlinux.org>
2023-01-17 19:57:07 +0100

    1.3.1

--- src/main/scala/pw/aldum/scalawcats/Main.scala
+++ src/main/scala/pw/aldum/scalawcats/Main.scala
@@ -2,11 +2,10 @@ package pw.aldum
 package scalawcats
 
 @main def Main(args: String*): Unit =
-  import PrintableInstances.given
 
   val hello = "hello world"
   val x = 1
   println("─" * 100)
-  println(Printable.format(hello))
-  Printable.print(x)
+  println(hello)
+  println(x)
   println("─" * 100)
--- src/main/scala/pw/aldum/scalawcats/Printable.scala
+++ src/main/scala/pw/aldum/scalawcats/Printable.scala
@@ -2,15 +2,4 @@ package pw.aldum
 package scalawcats
 
 trait Printable[A]:
-  def format[A](a: A): String = a.toString
-
-object PrintableInstances:
-  given Printable[String] with
-    def format(s: String): String = s
-
-  given Printable[Int] with
-    def format(i: Int): String = i.toString
-
-object Printable:
-  def format[A: Printable](a: A) = summon[Printable[A]].format(a)
-  def print[A: Printable](a: A) = println(format(a))
+  def format[A](a: A): String
