66a266ee79a847aee2350f46922b5486b365c1f3
aldum <aldum@artixlinux.org>
2023-01-17 21:36:41 +0100

    1.5.2

--- src/main/scala/pw/aldum/scalawcats/Main.scala
+++ src/main/scala/pw/aldum/scalawcats/Main.scala
@@ -1,16 +1,18 @@
 package pw.aldum
 package scalawcats
 
-import cats.Show
-import cats.syntax.show.*
+import cats.Eq
+import cats.syntax.eq.*
 
 import java.util.Date
 
 @main def Main(args: String*): Unit =
   val fluffy = Cat("Fluffy", 3, "black")
 
-  val showInt = Show.apply[Int]
+  val eqInt = Eq[Int]
   println("─" * 100)
-  println(new Date().show)
-  println(fluffy.show)
+  println(eqInt.eqv(123, 123))
+  println(eqInt.eqv(123, 120))
+  println(120 === 123)
+  // println(120 === "123") // Found: ("123" : String)  |  Required: Int
   println("─" * 100)
