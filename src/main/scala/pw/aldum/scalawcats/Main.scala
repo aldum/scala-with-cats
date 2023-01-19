8d2e948f4512435984efa172e67060c2eb0da0a6
aldum <aldum@artixlinux.org>
2023-01-19 21:48:47 +0100

    expand from book

--- src/main/scala/pw/aldum/scalawcats/Main.scala
+++ src/main/scala/pw/aldum/scalawcats/Main.scala
@@ -5,14 +5,5 @@ import cats.Eq
 import cats.syntax.eq.*
 
 @main def Main(args: String*): Unit =
-  object intSetM extends SetUnionMonoid[Int]
-  val sue = intSetM.empty
-  val set1 = Set(1, 2, 3)
-  val set2 = Set(3, 5)
-  val set3 = Set(4, 10)
-  val app = intSetM.combine
   println("─" * 100)
-  println(app(set1, sue))
-  println(app(sue, set1))
-  println(app(set1, app(set2, set3)) == app(app(set1, set2), set3))
   println("─" * 100)
--- src/main/scala/pw/aldum/scalawcats/Monoid.scala
+++ src/main/scala/pw/aldum/scalawcats/Monoid.scala
@@ -20,7 +20,45 @@ object BoolOrMonoid extends Monoid[Boolean]:
   def combine(x: Boolean, y: Boolean): Boolean = x || y
   def empty: Boolean = false
 
+// exclusive or with identity false
+given booleanEitherMonoid: Monoid[Boolean] =
+  new Monoid[Boolean]:
+    def combine(a: Boolean, b: Boolean) =
+      (a && !b) || (!a && b)
+
+    def empty = false
+
+// exclusive nor
+given booleanXnorMonoid: Monoid[Boolean] =
+  new Monoid[Boolean]:
+    def combine(a: Boolean, b: Boolean) =
+      (!a || b) && (a || !b)
+
+    def empty = true
+
 // What monoids and semigroups are there for sets?
 trait SetUnionMonoid[A] extends Monoid[Set[A]]:
   def combine(x: Set[A], y: Set[A]): Set[A] = x union y
   def empty: Set[A] = Set.empty[A]
+
+// implicit def setUnionMonoid[A]: Monoid[Set[A]] =
+//   new Monoid[Set[A]] {
+//     def combine(a: Set[A], b: Set[A]) = a union b
+//     def empty = Set.empty[A]
+//   }
+
+given setUnionMonoid[A]: Monoid[Set[A]] =
+  new Monoid[Set[A]]:
+    def combine(a: Set[A], b: Set[A]) = a union b
+    def empty = Set.empty[A]
+
+// Set intersection forms a semigroup, but doesn’t form a monoid because it has
+// no identity element:
+trait SetIntersectionSemigroup[A] extends Semigroup[Set[A]]:
+  def combine(x: Set[A], y: Set[A]): Set[A] = x intersect y
+
+// symmetric difference (the union less the intersection) does also form a monoid
+given setDiffMonoid[A]: Monoid[Set[A]] =
+  new Monoid[Set[A]]:
+    def combine(a: Set[A], b: Set[A]) = (a diff b) union (b diff a)
+    def empty = Set.empty[A]
