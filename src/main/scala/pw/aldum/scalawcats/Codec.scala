9c05a664a3407852771a82b69cf21fdaeb717d23
aldum <aldum@artixlinux.org>
2023-01-27 23:57:23 +0100

    3.5.6.1

--- src/main/scala/pw/aldum/scalawcats/Codec.scala
+++ src/main/scala/pw/aldum/scalawcats/Codec.scala
@@ -1,10 +1,18 @@
 package pw.aldum
 package scalawcats
 
-trait Codec[A]:
+import cats.syntax.validated
+
+trait Codec[A] { self =>
   def encode(value: A): String
   def decode(value: String): A
-  def imap[B](dec: A => B, enc: B => A): Codec[B] = ???
+  def imap[B](dec: A => B, enc: B => A): Codec[B] =
+    new Codec[B]:
+      def encode(value: B): String =
+        self.encode(enc(value))
+      def decode(value: String): B =
+        dec(self.decode(value))
+}
 
 def encode[A](value: A)(using c: Codec[A]): String =
   c.encode(value)
--- src/main/scala/pw/aldum/scalawcats/Main.scala
+++ src/main/scala/pw/aldum/scalawcats/Main.scala
@@ -8,10 +8,18 @@ import cats.instances.function.* // for Functor
 import cats.syntax.functor.*     // for map
 
 @main def Main(args: String*): Unit =
-  import scalawcats.PrintableInstances.given
+  import scalawcats.given
   val x = 75
 
+  given dc: Codec[Double] =
+    summon[Codec[String]].imap(_.toDouble, _.toString)
+  given Codec[Box[Double]] =
+    dc.imap(Box.apply, (b: Box[Double]) => b.value)
+
   println("─" * x)
-  println(format(Box("hello world")))
-  println(format(Box(true)))
+  println(encode(3.14))
+  println(decode[Double]("2.71"))
+
+  println(encode(Box(123.4)))
+  println(decode[Box[Double]]("123.4"))
   println("─" * x)
