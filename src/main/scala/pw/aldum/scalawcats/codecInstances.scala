abe4f7c27f0eb62854826d228f5f1b2e69c853f4
aldum <aldum@artixlinux.org>
2023-01-27 23:26:59 +0100

    3.5.6 use summon

--- src/main/scala/pw/aldum/scalawcats/codecInstances.scala
+++ src/main/scala/pw/aldum/scalawcats/codecInstances.scala
@@ -3,14 +3,14 @@ package scalawcats
 
 // implicit val stringCodec: Codec[String] =
 //   new Codec[String]:
-given stringCodec: Codec[String] with
+given Codec[String] with
   def encode(value: String): String = value
   def decode(value: String): String = value
 
 // implicit val intCodec: Codec[Int] =
 given Codec[Int] =
-  stringCodec.imap(_.toInt, _.toString)
+  summon[Codec[String]].imap(_.toInt, _.toString)
 
 // implicit val booleanCodec: Codec[Boolean] =
 given Codec[Boolean] =
-  stringCodec.imap(_.toBoolean, _.toString)
+  summon[Codec[String]].imap(_.toBoolean, _.toString)
