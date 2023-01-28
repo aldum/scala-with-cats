628ca34a6784ee0ae303b1b2bf0bcc67fbb7643c
aldum <aldum@artixlinux.org>
2023-01-28 15:20:03 +0100

    4.4.2

--- src/main/scala/pw/aldum/scalawcats/Main.scala
+++ src/main/scala/pw/aldum/scalawcats/Main.scala
@@ -1,6 +1,16 @@
 package pw.aldum
 package scalawcats
 
+import cats.syntax.either.* // for asRight
+
+def countPositive(nums: List[Int]) =
+  // nums.foldLeft(Right(0)) { (accumulator, num) =>
+  nums.foldLeft(0.asRight) { (accumulator, num) =>
+    if(num > 0)
+      accumulator.map(_ + 1)
+    else
+      Left("Negative. Stopping!")
+  }
 
 @main def Main(args: String*): Unit =
   import scalawcats.given
