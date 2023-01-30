87ab3059c0ae0d869c15f48ddb3230b33cbbbec3
aldum <aldum@artixlinux.org>
2023-01-30 17:04:09 +0100

    4.9.3 part 2 book

--- src/main/scala/pw/aldum/scalawcats/Ch4/RPN.scala
+++ src/main/scala/pw/aldum/scalawcats/Ch4/RPN.scala
@@ -2,12 +2,13 @@ package pw.aldum
 package scalawcats
 
 import cats.data.State
+import cats.syntax.all.*
+import cats.syntax.flatMap
 
 import scala.util.Try
 import scala.util.Failure
 import scala.util.Success
 
-import math.Integral.Implicits.infixIntegralOps
 
 object RPN:
 
@@ -36,7 +37,36 @@ object RPN:
         case Value(n) => (n :: oldStack , n)
         case Op(op)   =>
           val newStack = oldStack.drop(2)
-          val List(op1, op2) = oldStack.take(2)
+          // (having the second operand at the top of the stack)
+          val List(op2, op1) = oldStack.take(2)
           val result   = op.apply(op1, op2)
           (result :: newStack, result)
+    }
+
+  def evalOneBook(sym: String): CalcState[Int] =
+    sym match
+      case "+" => operator(_ + _)
+      case "-" => operator(_ - _)
+      case "*" => operator(_ * _)
+      case "/" => operator(_ / _)
+      case num => operand(num.toInt)
+
+  private def operand(num: Int): CalcState[Int] =
+    State[List[Int], Int] { stack =>
+      (num :: stack, num)
+    }
+
+  private def operator(func: (Int, Int) => Int): CalcState[Int] =
+    State[List[Int], Int] {
+      case b :: a :: tail =>
+        val ans = func(a, b)
+        (ans :: tail, ans)
+
+      case _ =>
+        sys.error("Fail!")
+    }
+
+  def evalAll(input: List[String]): CalcState[Int] =
+    input.foldLeft(State.pure(0)){ (a, b) =>
+      a.flatMap(_ => evalOne(b))
     }
--- src/main/scala/pw/aldum/scalawcats/Main.scala
+++ src/main/scala/pw/aldum/scalawcats/Main.scala
@@ -13,7 +13,15 @@ package scalawcats
     for
       _   <- evalOne("1")
       _   <- evalOne("2")
-      ans <- evalOne("+")
+      ans <- evalOne("-")
     yield ans
   println(program.runA(Nil).value)
+  val p2 = evalAll(List("1", "2", "+", "3", "*"))
+  println(p2.runA(Nil).value)
+  val biggerProgram = for
+    _   <- evalAll(List("1", "2", "+"))
+    _   <- evalAll(List("3", "4", "+"))
+    ans <- evalOne("*")
+  yield ans
+  println(biggerProgram.runA(Nil).value)
   println("─" * x)
