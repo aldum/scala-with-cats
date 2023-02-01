47f8df54f34da257e0823a80b49f36429b87173e
aldum <aldum@artixlinux.org>
2023-02-01 22:46:39 +0100

    4.10.1 tailRecM book

--- src/main/scala/pw/aldum/scalawcats/Ch4/treeMonad.scala
+++ src/main/scala/pw/aldum/scalawcats/Ch4/treeMonad.scala
@@ -9,20 +9,44 @@ import scala.annotation.tailrec
 
 given Monad[Tree] = new Monad[Tree]:
 
-  override def pure[A](a: A): Tree[A] =
+  def pure[A](a: A): Tree[A] =
     Leaf(a)
 
-  override def flatMap[A, B](t: Tree[A])(f: A => Tree[B]): Tree[B] =
+  def flatMap[A, B](t: Tree[A])(f: A => Tree[B]): Tree[B] =
     t match
       case Leaf(a)      => f(a)
       case Branch(l, r) => Branch(flatMap(l)(f), flatMap(r)(f))
 
-  // @tailrec
-  override def tailRecM[A, B](a: A)(f: A => Tree[Either[A, B]]): Tree[B] =
-    f(a) match
-      case Leaf(Left(l))  => tailRecM(l)(f)
-      case Leaf(Right(l)) => leaf(l)
-      case Branch(l, r)   =>
-        Branch(???, ???)
+  def tailRecM[A, B](arg: A)
+      (func: A => Tree[Either[A, B]]): Tree[B] =
+    @tailrec
+    def loop(
+          open: List[Tree[Either[A, B]]],
+          closed: List[Option[Tree[B]]]): List[Tree[B]] =
+      open match
+        case Branch(l, r) :: next =>
+          loop(l :: r :: next, None :: closed)
 
+        case Leaf(Left(value)) :: next =>
+          loop(func(value) :: next, closed)
 
+        case Leaf(Right(value)) :: next =>
+          loop(next, Some(pure(value)) :: closed)
+
+        case Nil =>
+          closed.foldLeft(Nil: List[Tree[B]]) { (acc, maybeTree) =>
+            maybeTree.map(_ :: acc).getOrElse {
+              val left :: right :: tail = acc: @unchecked
+              branch(left, right) :: tail
+            }
+          }
+
+    loop(List(func(arg)), Nil).head
+  end tailRecM
+
+  // def tailRecM[A, B](a: A)
+  //     (f: A => Tree[Either[A, B]]): Tree[B] =
+  //   flatMap( f(a) ) {
+  //     case Left(value) => tailRecM(value)(f)
+  //     case Right(value) => Leaf(value)
+  //   }
--- src/main/scala/pw/aldum/scalawcats/Main.scala
+++ src/main/scala/pw/aldum/scalawcats/Main.scala
@@ -8,26 +8,5 @@ import cats.syntax.all.toFunctorOps
   import scalawcats.given
   val x = 75
 
-  import Tree.*
-
-  val incrLeaf = (i: Int) => Leaf(i + 1)
-  val dupLeaf = (i: Int) => Leaf(i * 2)
-  val tsLeaf = (i: Int) => Leaf(s""""$i"""")
-
   println("─" * x)
-  println(leaf(4))
-  println(leaf(4).flatMap(incrLeaf))
-  val t = Branch(
-      Branch(Leaf(2)
-          , Branch(Leaf(3), Leaf(4)) )
-      , Leaf(5)
-    )
-  println(t)
-  println(t.flatMap(dupLeaf))
-  println(t.flatMap(tsLeaf))
-  val ttt = for
-    n  <- t
-  yield n + 10
-  println(ttt)
-
   println("─" * x)
