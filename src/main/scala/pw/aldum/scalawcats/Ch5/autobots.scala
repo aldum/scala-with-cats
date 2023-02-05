87f9cbe8b9240eb0a7641803ea5fbfe15a4a39f7
aldum <aldum@artixlinux.org>
2023-02-05 22:01:28 +0100

    5.4 part 2

--- src/main/scala/pw/aldum/scalawcats/Ch5/autobots.scala
+++ src/main/scala/pw/aldum/scalawcats/Ch5/autobots.scala
@@ -2,11 +2,25 @@ package pw.aldum
 package scalawcats
 
 import cats.data.EitherT
+import cats.syntax.applicative.*
+import cats.instances.future.*
+import cats.instances.either.*
 
 import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 // type Response[A] = Future[Either[String, A]]
 type Response[A] = EitherT[Future, String, A]
 
+val powerLevels = Map(
+  "Jazz"      -> 6,
+  "Bumblebee" -> 8,
+  "Hot Rod"   -> 10
+)
+
 def getPowerLevel(autobot: String): Response[Int] =
-  ???
+  EitherT(Future(
+    powerLevels.get(autobot) match
+      case Some(v) => Right(v)
+      case None    => Left("not found")
+  ))
--- src/main/scala/pw/aldum/scalawcats/Main.scala
+++ src/main/scala/pw/aldum/scalawcats/Main.scala
@@ -4,9 +4,16 @@ package scalawcats
 import cats.syntax.flatMap.*
 import cats.syntax.all.toFunctorOps
 
+import scala.concurrent.ExecutionContext.Implicits.global
+
 @main def Main(args: String*): Unit =
   import scalawcats.given
   val x = 75
 
   println("─" * x)
+  val f1 = getPowerLevel("asdf").value
+  val f2 = getPowerLevel("Jazz").value
+  println(f1.value)
+  println(f2.value)
+
   println("─" * x)
