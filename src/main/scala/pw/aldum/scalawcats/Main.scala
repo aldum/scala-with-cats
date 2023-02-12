8241a6865d6b0ba24d53a605c36659dcb2926f15
aldum <aldum@artixlinux.org>
2023-02-12 15:44:36 +0100

    7.1.2

--- src/main/scala/pw/aldum/scalawcats/Main.scala
+++ src/main/scala/pw/aldum/scalawcats/Main.scala
@@ -1,23 +1,23 @@
 package pw.aldum
 package scalawcats
 
-import cats.syntax.flatMap.*
-import cats.syntax.all.toFunctorOps
-
-import scala.concurrent.ExecutionContext.Implicits.global
-
 @main def Main(args: String*): Unit =
   import scalawcats.given
   val x = 75
 
   println("─" * x)
 
-  val f1 = tacticalReport("Ironhide", "Jazz")
-  val f2 = tacticalReport("Jazz", "Bumblebee")
-  val f3 = tacticalReport("Hot Rod", "Bumblebee")
-
-  println(f1)
-  println(f2)
-  println(f3)
+  val l = List(1, 2, 3)
+  println(l.foldLeft(0)(_ - _))
+  println(l.foldRight(0)(_ - _))
+  println("─" * (x / 5))
+  val ll: List[List[Int]] = l.map(List.apply(_))
+  val nill = List.empty[List[Int]]
+  println(ll.foldLeft(nill)((a, i) => i :: a))
+  println(ll.foldRight(nill)((i, a) => i :: a))
+  println("─" * (x / 5))
+  val nil = List.empty[Int]
+  println(l.foldLeft(nil)((a, i) => i :: a))
+  println(l.foldRight(nil)((i, a) => i :: a))
 
   println("─" * x)
