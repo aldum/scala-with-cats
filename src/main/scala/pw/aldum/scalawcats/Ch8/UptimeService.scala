6c73d2c165ddada199d77e2e9352fca089965cb3
aldum <aldum@artixlinux.org>
2023-02-16 07:41:04 +0100

    8.2 context bound syntax

--- src/main/scala/pw/aldum/scalawcats/Ch8/UptimeService.scala
+++ src/main/scala/pw/aldum/scalawcats/Ch8/UptimeService.scala
@@ -5,6 +5,7 @@ import cats.Applicative
 import cats.syntax.functor.*   // for map
 import cats.syntax.traverse.*  // for traverse
 
-class UptimeService[F[_]](client: UptimeClient[F])(using Applicative[F]):
+// class UptimeService[F[_]](client: UptimeClient[F])(using Applicative[F]):
+class UptimeService[F[_]: Applicative](client: UptimeClient[F]):
   def getTotalUptime(hostnames: List[String]): F[Int] =
     hostnames.traverse(client.getUptime).map(_.sum)
