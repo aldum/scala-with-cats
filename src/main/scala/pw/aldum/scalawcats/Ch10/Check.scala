d1507c842448eab66ce890bdf5cf7df7009d8bcf
aldum <aldum@artixlinux.org>
2023-02-26 18:24:13 +0100

    10.3.3 and book 1

--- src/main/scala/pw/aldum/scalawcats/Ch10/Check.scala
+++ src/main/scala/pw/aldum/scalawcats/Ch10/Check.scala
@@ -4,20 +4,17 @@ package scalawcats
 import cats.Semigroup
 import cats.instances.list.* // for Semigroup
 import cats.syntax.semigroup.* // for |+|
+import cats.syntax.either.*
 
-trait Check[E, A]:
-  def apply(value: A): Either[E, A]
-  def and(that: Check[E, A]): Check[E, A]
-
-final case class CheckF[E: Semigroup, A](func: A => Either[E, A]) extends Check[E, A]:
+final case class CheckF[E: Semigroup, A](func: A => Either[E, A]):
   def apply(a: A): Either[E, A] =
     func(a)
 
-  def and(that: Check[E, A]): Check[E, A] =
+  infix def and(that: CheckF[E, A]): CheckF[E, A] =
     CheckF((a: A) =>
-      (this.apply(a), that.apply(a)) match
-        case (Right(a), Right(_)) => Right(a)
+      (this(a), that(a)) match
+        case (Right(a), Right(_)) => a.asRight
         case (Left(e1), Left(e2)) => Left(e1 |+| e2)
-        case (Left(e1), Right(_)) => Left(e1)
-        case (Right(_), Left(e2)) => Left(e2)
+        case (Left(e1), Right(_)) => e1.asLeft
+        case (Right(_), Left(e2)) => e2.asLeft
     )
--- src/main/scala/pw/aldum/scalawcats/Main.scala
+++ src/main/scala/pw/aldum/scalawcats/Main.scala
@@ -1,6 +1,8 @@
 package pw.aldum
 package scalawcats
 
+import cats.syntax.either.*
+
 import scala.concurrent.Await
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -8,10 +10,27 @@ import scala.concurrent.duration.*
 
 @main def Main(args: String*): Unit =
   import scalawcats.given
-  val x = 75
 
   def fPrintln[A](f: Future[A]): Unit =
     println(Await.result(f, 1.second))
+  val x = 75
+
+  val a: CheckF[List[String], Int] =
+    CheckF { v =>
+      if (v > 2) v.asRight
+      else List("Must be > 2").asLeft
+    }
+
+  val b: CheckF[List[String], Int] =
+    CheckF { v =>
+      if (v < -2) v.asRight
+      else List("Must be < -2").asLeft
+    }
+
+  val check: CheckF[List[String], Int] =
+    a and b
 
   println("─" * x)
+  println(check(5))
+  println(check(0))
   println("─" * x)
