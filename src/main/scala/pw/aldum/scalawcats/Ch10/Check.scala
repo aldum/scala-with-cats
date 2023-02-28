dd99792ee3e9a091f5b7ebfb4a618f7c15bedd6e
aldum <aldum@artixlinux.org>
2023-02-28 13:34:31 +0100

    10.3.3 book 2

--- src/main/scala/pw/aldum/scalawcats/Ch10/Check.scala
+++ src/main/scala/pw/aldum/scalawcats/Ch10/Check.scala
@@ -18,3 +18,33 @@ final case class CheckF[E: Semigroup, A](func: A => Either[E, A]):
         case (Left(e1), Right(_)) => e1.asLeft
         case (Right(_), Left(e2)) => e2.asLeft
     )
+
+
+sealed trait Check[E, A]:
+  import Check.*
+
+  def and(that: Check[E, A]): Check[E, A] =
+    And(this, that)
+
+  def apply(a: A)(implicit s: Semigroup[E]): Either[E, A] =
+    this match
+      case Pure(func) =>
+        func(a)
+
+      case And(left, right) =>
+        (left(a), right(a)) match
+          case (Left(e1),  Left(e2))  => (e1 |+| e2).asLeft
+          case (Left(e),   Right(_))  => e.asLeft
+          case (Right(_),  Left(e))   => e.asLeft
+          case (Right(_), Right(_)) => a.asRight
+object Check {
+  final case class And[E, A](
+    left: Check[E, A],
+    right: Check[E, A]) extends Check[E, A]
+
+  final case class Pure[E, A](
+    func: A => Either[E, A]) extends Check[E, A]
+
+  def pure[E, A](f: A => Either[E, A]): Check[E, A] =
+    Pure(f)
+}
