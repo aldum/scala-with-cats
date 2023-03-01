d78603057e09551820917784b1860f2ee751c260
aldum <aldum@artixlinux.org>
2023-03-01 16:08:03 +0100

    11.5.2 scala 3

--- src/main/scala/pw/aldum/scalawcats/Ch11/KeyValueStore.scala
+++ src/main/scala/pw/aldum/scalawcats/Ch11/KeyValueStore.scala
@@ -24,17 +24,30 @@ given [K, V]: KeyValueStore[Map] =
       f.values.toList
 
 
-implicit class KvsOps[F[_,_], K, V](f: F[K, V]):
-  def put(key: K, value: V)
-        (implicit kvs: KeyValueStore[F]): F[K, V] =
+// extension [F[_,_], K, V](f: F[K, V])
+//   def put(key: K, value: V)
+//          (using kvs: KeyValueStore[F]): F[K, V] =
+//     kvs.put(f)(key, value)
+
+//   def get(key: K)(using kvs: KeyValueStore[F]): Option[V] =
+//     kvs.get(f)(key)
+
+//   def getOrElse(key: K, default: V)
+//                (using kvs: KeyValueStore[F]): V =
+//     kvs.getOrElse(f)(key, default)
+
+//   def values(using kvs: KeyValueStore[F]): List[V] =
+//     kvs.values(f)
+
+extension [F[_,_], K, V](f: F[K, V])(using kvs: KeyValueStore[F])
+  def put(key: K, value: V): F[K, V] =
     kvs.put(f)(key, value)
 
-  def get(key: K)(implicit kvs: KeyValueStore[F]): Option[V] =
+  def get(key: K): Option[V] =
     kvs.get(f)(key)
 
-  def getOrElse(key: K, default: V)
-        (implicit kvs: KeyValueStore[F]): V =
+  def getOrElse(key: K, default: V): V =
     kvs.getOrElse(f)(key, default)
 
-  def values(implicit kvs: KeyValueStore[F]): List[V] =
+  def values: List[V] =
     kvs.values(f)
