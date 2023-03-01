497b56a6fbd59c872d065e4381b7d6fab45984ca
aldum <aldum@artixlinux.org>
2023-03-01 13:30:04 +0100

    11.3.2

---
+++ src/main/scala/pw/aldum/scalawcats/Ch11/BoundedSemiLattice.scala
@@ -0,0 +1,20 @@
+package pw.aldum
+package scalawcats
+
+import cats.kernel.CommutativeMonoid
+
+trait BoundedSemiLattice[A]:
+  def combine(a1: A, a2: A): A
+  def empty: A
+
+given BoundedSemiLattice[Int] with
+  override def combine(a1: Int, a2: Int): Int =
+    a1 max a2
+
+  val empty = 0
+
+given BoundedSemiLattice[Set[?]] with
+  override def combine(a1: Set[?], a2: Set[?]): Set[?] =
+    a1 concat a2
+
+  val empty = Set.empty
