237d7480563ddd98d5a91f77ed79f2f009bec306
aldum <aldum@artixlinux.org>
2023-03-01 14:17:39 +0100

    11.3.3 book fix

--- src/main/scala/pw/aldum/scalawcats/Ch11/GCounter.scala
+++ src/main/scala/pw/aldum/scalawcats/Ch11/GCounter.scala
@@ -1,23 +1,17 @@
 package pw.aldum
 package scalawcats
 
-import cats.syntax.monoid.*
+import cats.kernel.CommutativeMonoid
+import cats.syntax.semigroup.* // for |+|
+import cats.syntax.foldable.*  // for combineAll
 
-final case class GCounter[V](counters: Map[String, V])(using bsl: BoundedSemiLattice[V]):
-  def increment(machine: String, amount: V) =
-    val value = bsl.combine(
-      counters.getOrElse(machine, bsl.empty),
-      amount,
-    )
+final case class GCounter[V](counters: Map[String, V]):
+  def increment(machine: String, amount: V)(using m: CommutativeMonoid[V]) =
+    val value = amount |+| counters.getOrElse(machine, m.empty)
     GCounter(counters.updated(machine, value))
 
-  infix def merge(that: GCounter[V]): GCounter[V] =
-    val merged = that.counters ++ counters.map {
-      case (m, a) =>
-        m -> bsl.combine(a, that.counters.getOrElse(m, bsl.empty))
-    }
+  infix def merge(that: GCounter[V])(using bsl: BoundedSemiLattice[V]): GCounter[V] =
+    GCounter(counters |+| that.counters)
 
-    GCounter(merged)
-
-  def total: V =
-    counters.values.fold(bsl.empty)(bsl.combine)
+  def total(using m: CommutativeMonoid[V]): V =
+    counters.values.toList.combineAll
